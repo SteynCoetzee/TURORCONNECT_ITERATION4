56be1cc1139061d214223df01c519cd3fa34f462
SteynCoetzee <steyncoetzee77@gmail.com>
2026-05-10 09:43:48 +0200

    Fix: IT4-only routes and remove missing service dependencies from home

--- tutorconnect-frontend/src/app/components/home/home.component.ts
+++ tutorconnect-frontend/src/app/components/home/home.component.ts
@@ -3,9 +3,7 @@ import { CommonModule, DatePipe, NgClass } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../../services/auth.service';
-import { ModuleService } from '../../services/module.service';
-import { AnnouncementService } from '../../services/announcement.service';
-import { Module, Announcement, Testimonial } from '../../models/models';
+import { Module, Testimonial } from '../../models/models';
 import { environment } from '../../../environments/environment';
 
 interface MediaContent { media_ID: number; media_Name: string; media_Address: string; }
@@ -21,8 +19,6 @@ export class HomeComponent implements OnInit {
   userName = '';
   role = '';
   modules: Module[] = [];
-  announcements: Announcement[] = [];
-  recentAnnouncements: Announcement[] = [];
   mediaItems: MediaContent[] = [];
   testimonials: Testimonial[] = [];
 
@@ -30,8 +26,6 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private moduleService: ModuleService,
-    private announcementService: AnnouncementService,
     private http: HttpClient
   ) {}
 
@@ -39,21 +33,14 @@ export class HomeComponent implements OnInit {
     this.userName = this.authService.getCurrentUserName();
     this.role = this.authService.getCurrentUserRole();
 
-    this.moduleService.getModules().subscribe({ next: (data) => { this.modules = data; }, error: () => {} });
-    this.announcementService.getAnnouncements().subscribe({
-      next: (data) => {
-        this.announcements = data;
-        this.recentAnnouncements = data.slice(0, 3);
-      },
-      error: () => {}
+    this.http.get<Module[]>(`${this.apiUrl}/Modules`).subscribe({
+      next: (data) => { this.modules = data; }, error: () => {}
     });
     this.http.get<MediaContent[]>(`${this.apiUrl}/AdminContent/media`).subscribe({
-      next: (data) => { this.mediaItems = data; },
-      error: () => {}
+      next: (data) => { this.mediaItems = data; }, error: () => {}
     });
     this.http.get<Testimonial[]>(`${this.apiUrl}/Testimonials/approved`).subscribe({
-      next: (data) => { this.testimonials = data.slice(0, 4); },
-      error: () => {}
+      next: (data) => { this.testimonials = data.slice(0, 4); }, error: () => {}
     });
   }
 
